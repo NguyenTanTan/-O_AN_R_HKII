54928a541c301a4af1add8b06431b071611a82f7
ADMIN <nguyenthanhtan.140103@gmail.com>
2022-06-02 12:04:46 +0700

    2/6

--- R-exercises 6.R
+++ R-exercises 6.R
@@ -1,3 +1,6 @@
+## Bài 1:
+#Với tập dữ liệu Temperature.txt hãy Vẽ biểu đồ dữ liệu nhiệt độ so với thời gian cho từng trạm và lưu
+#đồ thị dưới dạng tệp jpg
 setwd("C:/Dữ liệu/RBook/")
 data<-read.table("Temperature.txt", header=TRUE)
 data
@@ -9,11 +12,11 @@ allyear
 for (i in 1:30){ 
   station.i<-allstation[i]
   for (j in 1:16){
-  year.j<-allyear[j]
-  data.i <- data[data$Station == station.i & data$Year==year.j, ]
-  YourFileName <- paste(station.i,year.j, ".jpg", sep = "")
-  jpeg(file = YourFileName)
-  plot(x = data.i$Month, 
+    year.j<-allyear[j]
+    data.i <- data[data$Station == station.i & data$Year==year.j, ]
+    YourFileName <- paste(station.i,year.j, ".jpg", sep = "")
+    jpeg(file = YourFileName)
+    plot(x = data.i$Month, 
        y = data.i$Temperature,
        xlab = "Month",
        ylab = "Temperature", main = paste(station.i,year.j))
